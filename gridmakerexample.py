b92abb2a3063d4a1a4945aa31f9855874c7d407d
JVN <johnvnettady@gmail.com>
2023-08-14 18:20:03 +0530

    added some help

--- gridmakerexample.py
+++ gridmakerexample.py
@@ -2,7 +2,7 @@ from gridmaker import create_grid
 
 grid = [[0,12],['adada',5656],['dafa','ffffffjjj']]
 
-tilesize=(10, 4)
+tilesize=(10, 4) #First value is for x axis specify the max length of the tiles , second is y for the height
 
 tablestr = create_grid(grid,tilesize)
 
